5a1560030131df3ce8ef5a1b9020d2148c2babfb
uiw6unoh <uiw6unoh@hubilon.com>
2022-12-02 16:48:38 +0900

    221202 환경설정, 서버 잘 돌아가는지 확인

--- src/main/java/jpabook/jpashop/MemberRepository.java
+++ src/main/java/jpabook/jpashop/MemberRepository.java
@@ -7,7 +7,13 @@ import javax.persistence.PersistenceContext;
 
 @Repository
 public class MemberRepository {
-
     @PersistenceContext
-    private EntityManager em;
+    EntityManager em;
+    public Long save(Member member) {
+        em.persist(member);
+        return member.getId();
+    }
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+    }
 }
