f9254dfa673879aa76ee1ef5b1189068f3ad7007
uiw6unoh <uiw6unoh@hubilon.com>
2022-12-02 10:05:47 +0900

    221202 인텔리제이 환경설정

---
+++ src/main/java/jpabook/jpashop/MemberRepository.java
@@ -0,0 +1,13 @@
+package jpabook.jpashop;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+}
