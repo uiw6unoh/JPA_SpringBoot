14d69569bfeb5a775de396f7daa4d083d6d18c24
uiw6unoh <uiw6unoh@naver.com>
2022-12-05 16:56:01 +0900

    221205 도메인 분석 설계

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,12 +1,10 @@
 package jpabook.jpashop.domain;
-
-import jpabook.jpashop.domain.item.Item;
 import lombok.Getter;
 import lombok.Setter;
-
+import jpabook.jpashop.domain.item.Item;
 import javax.persistence.*;
-
 @Entity
+@Table(name = "order_item")
 @Getter @Setter
 public class OrderItem {
 
@@ -14,15 +12,13 @@ public class OrderItem {
     @Column(name = "order_item_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "item_id")
-    private Item item;
+    private Item item; //주문 상품
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "order_id")
-    private Order order;
-
-    private int orderPrice; // 주문 가격
-    private int count;  // 주문 수량
-
+    private Order order; //주문
+    private int orderPrice; //주문 가격
+    private int count; //주문 수량
 }
