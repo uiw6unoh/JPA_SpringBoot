52779e71c838821beae1ed08ddc87037c82c7ce6
uiw6unoh <uiw6unoh@hubilon.com>
2022-12-05 16:49:13 +0900

    221205 도메인 분석 설계

--- src/main/java/jpabook/jpashop/domain/Delivery.java
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -1,10 +1,7 @@
 package jpabook.jpashop.domain;
-
 import lombok.Getter;
 import lombok.Setter;
-
 import javax.persistence.*;
-
 @Entity
 @Getter @Setter
 public class Delivery {
@@ -13,12 +10,12 @@ public class Delivery {
     @Column(name = "delivery_id")
     private Long id;
 
-    @OneToOne(mappedBy = "delivery")
+    @OneToOne(mappedBy = "delivery", fetch = FetchType.LAZY)
     private Order order;
 
     @Embedded
     private Address address;
 
     @Enumerated(EnumType.STRING)
-    private DeliveryStatus status;  // [READY, COMP]
+    private DeliveryStatus status; //ENUM [READY(준비), COMP(배송)]
 }
