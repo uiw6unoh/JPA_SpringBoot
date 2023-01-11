cd8bb2c901bf35878609061b17059b315f662bb6
uiw6unoh <uiw6unoh@naver.com>
2023-01-11 11:51:40 +0900

    230111 쿼리 메소드 기능(4)

--- Data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ Data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -17,6 +17,8 @@ import static org.assertj.core.api.Assertions.*;
 class MemberJpaRepositoryTest {
 
     @Autowired MemberJpaRepository memberJpaRepository;
+    @Autowired
+    private MemberRepository memberRepository;
 
     @Test
     public void testMember() {
@@ -84,5 +86,32 @@ class MemberJpaRepositoryTest {
         List<Member> result = memberJpaRepository.findByUsername("AAA");
         Member findMember = result.get(0);
         assertThat(findMember).isEqualTo(m1);
+    }
+
+    @Test
+    public void paging() {
+        // given
+        memberJpaRepository.save((new Member("member1", 10)));
+        memberJpaRepository.save((new Member("member2", 10)));
+        memberJpaRepository.save((new Member("member3", 10)));
+        memberJpaRepository.save((new Member("member4", 10)));
+        memberJpaRepository.save((new Member("member5", 10)));
+
+        int age = 10;
+        int offset = 0;
+        int limit = 3;
+
+        // when
+        List<Member> members = memberJpaRepository.findByPage(age, offset, limit);
+        long totalCount = memberJpaRepository.totalCount(age);
+
+        // 페이지 계산 공식 적용...
+        // totalPage = totalCount / size ...
+        // 마지막 페이지 ...
+        // 최초 페이지 ...
+
+        // then
+        assertThat(members.size()).isEqualTo(3);
+        assertThat(totalCount).isEqualTo(5);
     }
 }
